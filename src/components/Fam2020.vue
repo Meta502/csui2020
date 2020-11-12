83bc5e0c96d19f2a1bc2222d2689183c6dfc6b35
Adrian Ardizza <adrian.ardizza@gmail.com>
2020-11-12 10:05:35 +0700

    Responsive searchbar, background

--- src/components/Fam2020.vue
+++ src/components/Fam2020.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
       <img src="static/svg/background-1.svg" class="background-1">
-      <img src="static/svg/background-2.svg" class="background-2">
+      <img src="static/svg/background-2.svg" class="background-2 d-none d-xl-block">
       <img src="static/svg/background-3.svg" class="background-3">
       <div class="container-fluid" id="app">
         <div class="jumbotron-fluid" style="min-height: 250px; margin-top: 75px">
@@ -11,15 +11,17 @@
                 <paginate-links class="text-center" for="all"></paginate-links>
             </div>
             <div class="text-center">
-              <input type="text" v-model="search">
+              <div>
+                <input class="rounded-pill" type="text" v-model="search" size="8" placeholder="Search">
+              </div>
             </div>
         </div>
-        <paginate class="row justify-content-center text-center mx-4" name="all" :list="fData" :per="50">
-            <div class="my-4 px-4" v-for="item in paginated('all')" v-bind:key="item" style="flex-basis: 12.5%">
-                <div class="image-card">
+        <paginate class="row justify-content-center p-0 mx-0 mx-md-4" name="all" :list="fData" :per="50">
+            <div class="my-5 px-4" v-for="item in paginated('all')" v-bind:key="item">
+                <div class="image-card mx-auto text-left">
                     <img class="rounded-circle profile-image" v-bind:src="`https://drive.google.com/uc?export=view&id=${item.foto}`">
                 </div>
-                <div class="name-subtitle p-3">
+                <div class="name-subtitle text-center p-3">
                     <p style="font-family: 'San Francisco'; font-weight: 700; letter-spacing: 0; font-size:1.1rem">
                       <small class="text-muted">[{{item.absen_pmb}}] </small>
                       <br>
@@ -70,10 +72,94 @@ ul.paginate-links li {
     display: inline-block;
     margin: 0 10px 0 10px
 }
+
+ul.paginate {
+    list-style-type: none;
+    padding: 0;
+}
+
 .profile-image {
   width:160px;
   height: 160px;
   object-fit: cover
 }
+
+.rounded-pill {
+  background-color: transparent;
+  border: solid 1px;
+  color: gray;
+  padding: 5px 0px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  margin: 4px 2px;
+  border-radius: 24px;
+  outline:none;
+  transition: padding 0.2s;
+}
+
+.rounded-pill:hover {
+  padding: 5px 45px;
+  transition: padding 0.2s
+}
+
+.rounded-pill:focus {
+  padding: 5px 45px;
+  transition: padding 0.2s
+}
+
+
+@media (max-width: 576px) { 
+    .background-1 {
+        top: 190px;
+        width: 80px!important;
+    }
+    
+    
+    .background-3 {
+        top:0px; right:0; width:130px!important
+    }
+}
+
+
+@media (min-width: 576px) {  
+    .background-1 {
+        top: 190px;
+        width: 190px!important;
+    }
+    
+    .background-2 {
+        top:0px; 
+        left:270px;
+        width:30px!important;
+    }
+    
+    .background-3 {
+        top:0px; right:0; width:190px!important
+    }
+}
+
+
+@media (min-width: 991px) {  
+
+.background-1 {
+    width: 250px!important
+}
+
+.background-2 {
+    width:160px!important;
+    display: block
+}
+
+.background-3 {
+   width:340px!important
+}
+
+}
+
+
+
+
+
 </style>
 
